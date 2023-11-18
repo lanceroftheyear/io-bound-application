44cd17412dbb89a265970a4c9d4ddea87bf8a69c
Songbum Bae <bae@ajio.co.jp>
2023-11-18 12:57:55 +0900

    changed postgresql ip

--- src/main/java/class101/foo/io/PostController.java
+++ src/main/java/class101/foo/io/PostController.java
@@ -1,6 +1,9 @@
 package class101.foo.io;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -11,22 +14,32 @@ public class PostController {
     @Autowired
     PostRepository postRepository;
 
+    private static final int PAGE_SIZE = 20;
+
     // 1. 글을 작성한다.
     @PostMapping("/post")
     public Post createPost(@RequestBody Post post) {
         return postRepository.save(post);
     }
 
-    // 2-1. 글 목록을 조회한다.
+    // 2 글 목록을 페이징하여 반환
     @GetMapping("/posts")
-    public List<Post> getPostList() {
-        return postRepository.findAll();
+    public Page<Post> getPostList(@RequestParam(defaultValue = "1") Integer page) {
+        return postRepository.findAll(
+                PageRequest.of(page - 1, PAGE_SIZE, Sort.by("id").descending())
+        );
     }
-    
-    // 2-2 글 목록을 페이징하여 반환
-    
+
     // 3. 글 번호로 조회
-    
+    @GetMapping("/post/{id}")
+    public Post getPostById(@PathVariable Long id) {
+        return postRepository.findById(id).get();
+    }
+
     // 4. 글 내용으로 검색 -> 해당 내용이 포함된 모든 글
+    @GetMapping("/search")
+    public List<Post> findPostsByContent(@PathVariable String content) {
+        return postRepository.findByContentContains(content);
+    }
 
 }
